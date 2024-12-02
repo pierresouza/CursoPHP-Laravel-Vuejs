b7042bacad39f6f999218367815bc19909e70193
pierresouza <pierre.s3@hotmail.com>
2024-12-02 17:06:08 -0300

    apply validation in fornecedor variable to see if it is empty

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -6,13 +6,13 @@ use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
-    public function fornecedor()
+    public function index()
     {
         $fornecedores = [
             0 => [
                 'nome' => 'Fornecedor 1',
                 'status' => 'N',
-                'cnpj' => '00.000.000/0000-00'
+                'cnpj' => ''
             ],
             1 => [
                 'nome' => 'Fornecedor 2',
--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -19,14 +19,8 @@
     <br>
     @isset($fornecedores[0]['cnpj'])
         CNPJ: {{ $fornecedores[0]['cnpj'] }}
+        @empty($fornecedores[0]['cnpj'])
+            - Vazio
+        @endempty
     @endisset
 @endisset
-
-
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-<h3>Existem vários fornecedores cadastrados</h3>
-@else
-<h3>Ainjda não existem fornecedores cadastrados</h3>
-@endif
