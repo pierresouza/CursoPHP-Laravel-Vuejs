0f6afe829dcac4a465d018a4005a2f439fcb2719
pierresouza <pierre.s3@hotmail.com>
2024-12-03 11:38:51 -0300

    feat: create switch/case for validate number area

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -12,13 +12,26 @@ class FornecedorController extends Controller
             0 => [
                 'nome' => 'Fornecedor 1',
                 'status' => 'N',
-                'cnpj' => ''
+                'cnpj' => '00',
+                'ddd' => '11',
+                'telefone' => '0000-0000',
             ],
             1 => [
                 'nome' => 'Fornecedor 2',
                 'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '85',
+                'telefone' => '0000-0001',
+            ],
+            2 => [
+                'nome' => 'Fornecedor 3',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '32',
+                'telefone' => '0000-0001',
             ],
         ];
+
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -17,10 +17,32 @@
     <br>
     Status: {{ $fornecedores[0]['status'] }}
     <br>
-    @isset($fornecedores[0]['cnpj'])
+    CNPJ:{{$fornecedores[0]['cnpj'] ?? 'Dado não preenchido'}}
+    <br>
+    Telefone: {{ $fornecedores[0]['ddd'] ?? '' }} {{ $fornecedores[0]['telefone'] ?? '' }}
+    <br>
+    @switch($fornecedores[0]['ddd'])
+        @case(11)
+            São Paulo - SP
+            @break
+        @case(32)
+            Juiz de fora - MG
+            @break
+        @case(85)
+            Fortaleza - CE
+            @break
+        @default
+            Estado não identificado
+    @endswitch
+   {{--
+   $variavel testada não estiver definida
+   ou
+   $variavel testada for nula
+    --}}
+    {{-- @isset($fornecedores[0]['cnpj'])
         CNPJ: {{ $fornecedores[0]['cnpj'] }}
         @empty($fornecedores[0]['cnpj'])
             - Vazio
         @endempty
-    @endisset
+    @endisset --}}
 @endisset
